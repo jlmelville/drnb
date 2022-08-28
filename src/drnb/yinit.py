bb38a018477ba53d351989fd1f4e824f820b3e62
James Melville <jlmelville@gmail.com>
2022-08-27 17:02:31 -0700

    add init code

---
+++ src/drnb/yinit.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+
+def scale_coords(coords, max_coord=10.0):
+    expansion = max_coord / np.abs(coords).max()
+    return (coords * expansion).astype(np.float32)
+
+
+def noisy_scale_coords(coords, max_coord=10.0, noise=0.0001):
+    return scale_coords(coords, max_coord=max_coord) + add_noise(coords, noise=noise)
+
+
+def add_noise(coords, noise=0.0001, seed=None):
+    rng = np.random.default_rng(seed=seed)
+    return coords + rng.normal(scale=noise, size=coords.shape).astype(np.float32)
