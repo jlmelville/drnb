1a422c41c327db50c61f2d7607564d352a94a735
James Melville <jlmelville@gmail.com>
2022-09-10 16:55:59 -0700

    new defaults

--- src/drnb/io/dataset.py
+++ src/drnb/io/dataset.py
@@ -1,6 +1,7 @@
+import pathlib
 from dataclasses import dataclass
 
-from drnb.io import FileExporter, get_xy, read_data
+from drnb.io import DATA_ROOT, FileExporter, get_xy, read_data
 from drnb.util import get_method_and_args, get_multi_config
 
 
@@ -15,24 +16,67 @@ def read_datax(dataset, data_path=None, sub_dir="xy", verbose=False):
     )
 
 
-def read_datay(dataset, data_path=None, sub_dir="xy", verbose=False, x=None):
+def read_datay(
+    dataset,
+    data_path=None,
+    sub_dir="xy",
+    verbose=False,
+    target_suffix="y",
+    x=None,
+    data_suffix="",
+):
     try:
         y = read_data(
-            dataset, suffix="y", data_path=data_path, sub_dir=sub_dir, verbose=verbose
+            dataset,
+            suffix=target_suffix,
+            data_path=data_path,
+            sub_dir=sub_dir,
+            verbose=verbose,
         )
     except FileNotFoundError:
         if x is None:
             x = read_data(
-                dataset, suffix="", data_path=data_path, sub_dir=sub_dir, verbose=False
+                dataset,
+                suffix=data_suffix,
+                data_path=data_path,
+                sub_dir=sub_dir,
+                verbose=False,
             )
         y = range(x.shape[0])
     return y
 
 
-def read_dataxy(dataset, data_path=None, sub_dir="xy", verbose=False):
+def read_dataxy(
+    dataset,
+    data_path=None,
+    sub_dir="xy",
+    verbose=False,
+):
     x = read_datax(dataset, data_path=data_path, sub_dir=sub_dir, verbose=verbose)
     y = read_datay(dataset, data_path=data_path, sub_dir=sub_dir, verbose=verbose, x=x)
     return x, y
+
+
+def read_dataset(
+    dataset,
+    data_path=None,
+    sub_dir="data",
+    data_suffix="data",
+    target_suffix="target",
+    verbose=False,
+):
+    data = read_data(
+        dataset, suffix=data_suffix, data_path=data_path, sub_dir=sub_dir, verbose=False
+    )
+    target = read_datay(
+        dataset,
+        data_path=data_path,
+        sub_dir=sub_dir,
+        verbose=verbose,
+        x=data,
+        target_suffix=target_suffix,
+    )
+    return data, target
 
 
 class XImporter:
@@ -47,13 +91,25 @@ class XImporter:
 
 @dataclass
 class DatasetImporter:
+    data_path: pathlib.Path = DATA_ROOT
+    sub_dir: str = "data"
+    data_suffix: str = "data"
+    target_suffix: str = "target"
+
     @classmethod
     def new(cls, **kwargs):
         return cls(**kwargs)
 
+    # pylint: disable=unused-argument
     def import_data(self, name, x=None, y=None):
-        x, y = read_dataxy(name)
-        return x, y
+        data, target = read_dataset(
+            name,
+            data_path=self.data_path,
+            sub_dir=self.sub_dir,
+            data_suffix=self.data_suffix,
+            target_suffix=self.target_suffix,
+        )
+        return data, target
 
 
 def create_dataset_importer(x=None, import_kwargs=None):
