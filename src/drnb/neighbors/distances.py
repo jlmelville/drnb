a48e61c940c5bd9c575977d9c59edfec7a0ff2c9
James Melville <jlmelville@gmail.com>
2022-10-02 13:32:19 -0700

    create random distances

---
+++ src/drnb/neighbors/distances.py
@@ -0,0 +1,22 @@
+import numpy as np
+from numba import jit, prange
+
+from drnb.distances import distance_function
+
+
+def neighbor_distances(data, idx, distance):
+    dist_fun = distance_function(distance)
+    return _neighbor_distances(data, idx, dist_fun)
+
+
+@jit(nopython=True, parallel=True)
+def _neighbor_distances(data, idx, dist_fun):
+    n_items = data.shape[0]
+    n_neighbors = idx.shape[1]
+    distances = np.empty(idx.shape, dtype=np.float32)
+    # pylint: disable=not-an-iterable
+    for i in prange(n_items):
+        idxi = idx[i]
+        for j in range(n_neighbors):
+            distances[i, j] = dist_fun(data[i], data[idxi[j]])
+    return distances
---
+++ src/drnb/neighbors/random.py
@@ -0,0 +1,30 @@
+import numpy as np
+
+from . import NearestNeighbors
+from .distances import neighbor_distances
+
+
+def random_neighbors(data, n_neighbors=None, distance="euclidean", random_state=42):
+    n_items = data.shape[0]
+    if n_neighbors is None:
+        n_neighbors = np.ceil(np.log(n_items)).astype(int)
+        print(n_neighbors)
+    idx = random_idx(n_items, n_neighbors, random_state=random_state)
+    dist = neighbor_distances(data, idx, distance)
+
+    # sort each row by ascending distance
+    dist_ind = dist.argsort()
+    dist = np.take_along_axis(dist, dist_ind, axis=1)
+    idx = np.take_along_axis(idx, dist_ind, axis=1)
+    return NearestNeighbors(idx=idx, dist=dist)
+
+
+def random_idx(n, n_neighbors, random_state=42):
+    result = np.empty((n, n_neighbors), dtype=int)
+    rng = np.random.default_rng(random_state)
+    for i in range(n):
+        result[i] = rng.choice(n - 1, size=n_neighbors, replace=False)
+        for j in range(n_neighbors):
+            if result[i, j] >= i:
+                result[i, j] += 1
+    return result
