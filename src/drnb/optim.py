90a1ef73d0031dd095ca54143879c656a3d26229
James Melville <jlmelville@gmail.com>
2022-12-29 21:31:42 -0800

    adabelief + warmup

--- src/drnb/optim.py
+++ src/drnb/optim.py
@@ -23,6 +23,8 @@ def create_opt(X, opt, optargs=None):
         optim = MomentumGH
     elif opt == "adam":
         optim = Adam
+    elif opt == "adabelief":
+        optim = AdaBelief
     else:
         raise ValueError(f"Unknown optimizer {opt}")
     return optim(**optargs)
@@ -33,6 +35,13 @@ def next_alpha(alpha, epoch, n_epochs):
     return alpha * (
         numba.float32(1.0) - (numba.float32(epoch) / numba.float32(n_epochs))
     )
+
+
+@numba.jit(nopython=True, fastmath=True)
+def warmup(alpha, epoch, n_warmup_epochs):
+    if epoch >= n_warmup_epochs:
+        return alpha
+    return alpha * numba.float32(epoch + 1) / numba.float32(n_warmup_epochs)
 
 
 @jitclass(
@@ -139,6 +148,7 @@ class MomentumGH:
         ("m", numba.float32[:, :]),
         ("v", numba.float32[:, :]),
         ("one", numba.float32),
+        ("warmup_alpha", numba.int32),
     ]
 )
 class Adam:
@@ -151,6 +161,7 @@ class Adam:
         beta1=0.9,
         beta2=0.999,
         eps=1e-8,
+        warmup_alpha=0,
     ):
         self.one = numba.float32(1.0)
         self.alpha = alpha
@@ -160,6 +171,7 @@ class Adam:
         self.beta11 = self.one - beta1
         self.beta21 = self.one - beta2
         self.eps = eps
+        self.warmup_alpha = warmup_alpha
         self.m = np.zeros((nobs, ndim), dtype=np.float32)
         self.v = np.zeros((nobs, ndim), dtype=np.float32)
 
@@ -172,6 +184,7 @@ class Adam:
             alpha = next_alpha(self.alpha, epoch, n_epochs)
         else:
             alpha = self.alpha
+        alpha = warmup(alpha, epoch, self.warmup_alpha)
         vbcorr = np.sqrt(self.one - self.beta2**e1)
         alpha *= vbcorr / (self.one - self.beta1**e1)
 
@@ -179,5 +192,67 @@ class Adam:
         self.m += self.beta11 * (grads - self.m)
         # self.v = self.beta2 * self.v + self.beta21 * grads * grads
         self.v += self.beta21 * (grads * grads - self.v)
+
+        return Y - alpha * (self.m / (np.sqrt(self.v) + self.eps / vbcorr))
+
+
+@jitclass(
+    [
+        ("nobs", numba.int32),
+        ("ndim", numba.int32),
+        ("alpha", numba.float32),
+        ("decay_alpha", numba.boolean),
+        ("beta1", numba.float32),
+        ("beta2", numba.float32),
+        ("beta11", numba.float32),
+        ("beta21", numba.float32),
+        ("eps", numba.float32),
+        ("m", numba.float32[:, :]),
+        ("v", numba.float32[:, :]),
+        ("one", numba.float32),
+        ("warmup_alpha", numba.int32),
+    ]
+)
+class AdaBelief:
+    def __init__(
+        self,
+        nobs,
+        ndim,
+        alpha=1e-3,
+        decay_alpha=False,
+        beta1=0.9,
+        beta2=0.999,
+        eps=1e-8,
+        warmup_alpha=0,
+    ):
+        self.one = numba.float32(1.0)
+        self.alpha = alpha
+        self.decay_alpha = decay_alpha
+        self.beta1 = beta1
+        self.beta2 = beta2
+        self.beta11 = self.one - beta1
+        self.beta21 = self.one - beta2
+        self.eps = eps
+        self.warmup_alpha = warmup_alpha
+        self.m = np.zeros((nobs, ndim), dtype=np.float32)
+        self.v = np.zeros((nobs, ndim), dtype=np.float32)
+
+    def opt(
+        self, Y: np.ndarray, grads: np.ndarray, epoch: int, n_epochs: int
+    ) -> np.ndarray:
+        e1 = numba.float32(epoch + 1)
+
+        if self.decay_alpha:
+            alpha = next_alpha(self.alpha, epoch, n_epochs)
+        else:
+            alpha = self.alpha
+        alpha = warmup(alpha, epoch, self.warmup_alpha)
+        vbcorr = np.sqrt(self.one - self.beta2**e1)
+        alpha *= vbcorr / (self.one - self.beta1**e1)
+
+        # self.m = self.beta1 * self.m + self.beta11 * grads
+        self.m += self.beta11 * (grads - self.m)
+        # self.v = self.beta2 * self.v + self.beta21 * grads * grads
+        self.v += self.beta21 * ((grads - self.m) ** 2 - self.v) + self.eps
 
         return Y - alpha * (self.m / (np.sqrt(self.v) + self.eps / vbcorr))
