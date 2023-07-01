40e712d5030689027b28a82093c53873d798d6c1
James Melville <jlmelville@gmail.com>
2023-07-01 08:50:00 -0700

    handle small dataset hub num

--- src/drnb/embed/umato.py
+++ src/drnb/embed/umato.py
@@ -3,6 +3,8 @@ import umato
 import drnb.embed
 from drnb.log import log
 
+DEFAULT_HUB_NUM = 300
+
 
 class Umato(drnb.embed.Embedder):
     def embed_impl(self, x, params, ctx=None):
@@ -11,6 +13,15 @@ class Umato(drnb.embed.Embedder):
 
 def umato_embed(x, params):
     log.info("Running UMATO")
+    # UMATO default hub_num is 300 and will fail if there are fewer than 300
+    # samples in the dataset. In this case, we will (somewhat arbitrarily) reduce the
+    # hub_num to data set size / 3 (https://github.com/hyungkwonko/umato/issues/21)
+    if x.shape[0] < DEFAULT_HUB_NUM:
+        small_hub_num = x.shape[0] // 3
+        hub_num = params.get("hub_num", DEFAULT_HUB_NUM)
+        if hub_num > x.shape[0]:
+            log.warning("Forcing hub_num to %d", small_hub_num)
+            params["hub_num"] = small_hub_num
     embedder = umato.UMATO(**params)
     embedded = embedder.fit_transform(x)
     log.info("Embedding completed")
