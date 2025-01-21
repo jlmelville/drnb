db7931b7c6085e9a012f851bd08858d9d0f1d240
James Melville <jlmelville@gmail.com>
2025-01-21 14:41:06 -0800

    control marker size/opacity and allow isotonic regression line

--- src/drnb/plot/rpdscatterplot.py
+++ src/drnb/plot/rpdscatterplot.py
@@ -5,6 +5,7 @@ import numpy as np
 import pandas as pd
 import seaborn as sns
 from matplotlib.axes import Axes
+from sklearn.isotonic import IsotonicRegression
 
 from drnb.embed.context import EmbedContext
 from drnb.eval.rpc import RandomPairCorrelEval
@@ -21,11 +22,18 @@ class RandomPairDistanceScatterplot:
             "euclidean").
         random_state: The random state to use when calculating the random pair distances
             (default None).
+        cex: The size of the points in the scatterplot (default 1).
+        alpha_scale: The alpha value to use for the points in the scatterplot (default
+            0.5).
+        show_isotonic: Whether to show the isotonic regression line (default False).
     """
 
     n_triplets_per_point: int = 5
     metric: str = "euclidean"
     random_state: int | None = None
+    cex: int = 1
+    alpha_scale: float = 0.5
+    show_isotonic: bool = False
 
     def rpc(self) -> RandomPairCorrelEval:
         """Create a RandomPairCorrelEval object."""
@@ -47,7 +55,30 @@ class RandomPairDistanceScatterplot:
     ) -> Axes | None:
         """Plot a scatterplot of the random pair distances for the embedded data."""
         vec = self.rpc().evaluatev(data, embedding_result["coords"], ctx)
-        plot = sns.scatterplot(x=vec[0], y=vec[1], s=1, alpha=0.5)
+        ambient_distances = vec[0]
+        embedded_distances = vec[1]
+        plot = sns.scatterplot(
+            x=ambient_distances,
+            y=embedded_distances,
+            s=self.cex,
+            alpha=self.alpha_scale,
+            edgecolor=None,
+        )
+
+        if self.show_isotonic:
+            iso = IsotonicRegression(increasing=True, out_of_bounds="clip")
+            iso.fit(ambient_distances, embedded_distances)
+
+            # Get prediction for sorted x values
+            ambient_sorted = np.sort(ambient_distances)
+            embedded_iso = iso.predict(ambient_sorted)
+
+            # Plot the isotonic regression line
+            plot.plot(
+                ambient_sorted, embedded_iso, "r-", lw=1, label="Isotonic Regression"
+            )
+            plot.legend()
+
         plot.set(title=str(self))
         plot.set_xlabel("ambient distances")
         plot.set_ylabel("embedded distances")
