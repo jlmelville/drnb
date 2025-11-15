2c7c294a457f31ea6105e5b7f2463fa5038f851b
James Melville <jlmelville@gmail.com>
2025-11-15 01:04:21 -0800

    document the export decisions in __init__.py

--- drnb-plugin-sdk/src/drnb_plugin_sdk/__init__.py
+++ drnb-plugin-sdk/src/drnb_plugin_sdk/__init__.py
@@ -1,4 +1,10 @@
-"""Public exports for the drnb plugin SDK."""
+"""Public exports for the drnb plugin SDK. Only the minimum necessary functions and
+types are exported that would be needed for plugins to interact with the drnb core.
+This is to make it clear which functions and structures would be needed to be
+implemented by plugins if they are unable to use this package directly, e.g. because
+they are using an incompatible version of Python or a different language altogether. As
+a result, the helper functions in this package are not exported here.
+"""
 
 from .protocol import (
     PROTOCOL_VERSION,
